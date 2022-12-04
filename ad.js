60918a752708ecdc43c393af617f28dd025a95b4
Ikromov-fs <nodirikromov10@gmail.com>
2022-12-04 13:53:50 +0300

    fs

---
+++ ad.js
@@ -0,0 +1,25 @@
+         // 1-masala
+
+var x = Number( prompt("x raqam kiriting"));
+var y = Number(prompt("y raqam kiriting"));
+var c = Number(prompt("x + y ni toping"));
+var z = x+y;
+if(c === z){
+    alert("to'gri malades");
+}
+else{
+    alert("boshqatdan urunib ko'ting");
+}
+
+
+// var massa = 70;
+// var buy = 180;
+
+// var bmi = (massa/buy)**2
+
+// if( bmi < 25){
+//     console.log("yaxshi");
+// }
+// else{
+//     console.log("yomon");
+// }
